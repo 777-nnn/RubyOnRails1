f63a5155d71b989cd53efb46575fd4b87c1b1591
777-nnn <natsu713@icloud.com>
2024-02-03 15:36:18 +0900

    複製されてしまった不要ファイルの削除

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,58 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      flash[:notice] = "スケジュールを新規登録しました"
+      redirect_to :posts
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      flash[:notice] = "スケジュール「#{@post.id}」の情報を更新しました"
+      redirect_to :posts
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "スケジュールを削除しました"
+    redirect_to :posts
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(
+      :title,
+      :start_date,
+      :end_date,
+      :is_all_day,
+      :schedule_memo
+    )
+  end
+
+end
